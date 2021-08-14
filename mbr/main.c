9e47805f38cb507a006f26e905a13d8126c34566
RenanPeres <renanperes99@usp.br>
2021-08-14 12:15:35 -0300

    Merge configuração local

--- mbr/main.c
+++ mbr/main.c
@@ -7,9 +7,9 @@
    SYSeg is distributed under the license GNU GPL v3, and is available
    at the official repository https://www.gitlab.com/monaco/syseg.
 
-   This file is part of <PROJECT>.
+   This file is part of soii21-rh_OS.
 
-   <PROJECT> is free software: you can redistribute it and/or modify
+   soii21-rh_OS is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
@@ -22,9 +22,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
-
 
 #include <mbr.h>
 
